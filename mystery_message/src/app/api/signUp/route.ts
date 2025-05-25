2fa00c346eb109be86f02793fa43f9283aefd414
Nishant kumar <nishant927472@gmail.com>
2025-05-25 22:03:34 +0530

    made signUp routes

--- mystery_message/src/app/api/signUp/route.ts
+++ mystery_message/src/app/api/signUp/route.ts
@@ -5,18 +5,89 @@ import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import { success } from "zod/v4";
 
-export async function POST(request: Request){
+export async function POST(request: Request) {
     await dbconnect()
 
     try {
-        const {username,email,password} = await request.json()
+        const { username, email, password } = await request.json()
+
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: true,
+                message: "username is already taken"
+            }, { status: 400 })
+        }
+
+        const existingUserByEmail = await UserModel.findOne({ email })
+
+        const varifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+
+        if (existingUserByEmail) {
+            if (existingUserByEmail.isVerified) {
+                return Response.json({
+                    success: false,
+                    message: "User already with this email"
+                }, { status: 400 })
+            }else{
+                const hashedPassword = await bcrypt.hash(password,10);
+                existingUserByEmail.password = hashedPassword;
+                existingUserByEmail.verifyCode = varifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() +  3600000) 
+
+                await existingUserByEmail.save();
+            }
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+
+            const expiryData = new Date()
+            expiryData.setHours(expiryData.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode: varifyCode,
+                verifyCodeExpiry: expiryData,
+                isVerified: false,
+                isAcceptingMessages: true,
+                message: []
+            })
+
+            await newUser.save()
+        }
+
+        //send varification code 
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            varifyCode
+        )
+
+        if (!emailResponse.success) {
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, { status: 500 })
+        }
+
+        return Response.json({
+            success: true,
+            message: "User registered successfully"
+        }, { status: 201 })
+
 
     } catch (error) {
-        console.log("Error registring User",error)
+        console.log("Error registring User", error)
         return Response.json(
             {
-                success:false,
-                message:"Error in registring registring"
+                success: false,
+                message: "Error in registring registring"
             },
             {
                 status: 500
