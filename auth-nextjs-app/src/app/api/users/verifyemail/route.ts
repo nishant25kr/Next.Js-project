9a21a934ef2255a555104412066837993185bd65
Nishant kumar <nishant927472@gmail.com>
2025-07-01 21:35:03 +0530

    fixed verifyemail route

--- auth-nextjs-app/src/app/api/users/verifyemail/route.ts
+++ auth-nextjs-app/src/app/api/users/verifyemail/route.ts
@@ -4,42 +4,51 @@ import { NextRequest, NextResponse } from "next/server";
 
 Connect()
 
-export async function POST(request:NextRequest) {
+export async function POST(request: NextRequest) {
     try {
 
         const reqbody = await request.json();
-        const {token} = reqbody
+        const { token } = reqbody
         console.log(token)
+        console.log("h1")
 
-        const user = await  User.findOne({verifyToken:token},{verifyTokenExpiry:{$gt:Date.now()}})
+        const user = await User.findOne({
+            verifyToken: token,
+            verifyTokenExpiry: { $gt: Date.now() }
+        });
 
-        if(!user){
+        console.log("h2")
+
+        if (!user) {
             return NextResponse.json(
-                {message:'User not found'},
-                {status:500}
+                { message: 'User not found' },
+                { status: 500 }
             )
         }
-        console.log(user)
+
+        console.log("h2")
+
 
         user.isVerfied = true;
-        user.verifyToken=undefined;
-        user.verifyTokenExpiry=undefined;
+        user.verifyToken = undefined;
+        user.verifyTokenExpiry = undefined;
+        console.log("h3")
 
         await user.save();
-
+        console.log("h4")
         return NextResponse.json(
             {
-                message:"user verified successfully",
-                success:true
+                message: "user verified successfully",
+                success: true
             }
         )
 
-        
 
-    } catch (error:any) {
+
+    } catch (error: any) {
         return NextResponse.json(
-            {error: error.message},
-            {status:500}
+            { error: error.message },
+            { status: 500 }
         )
     }
 }
